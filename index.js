28abafaa2317fd39f772b7284fe252b43da8af8d
ButterDebugger <34288129+ButterDebugger@users.noreply.github.com>
2024-01-10 21:50:12 -0500

    Fixed listening port

--- index.js
+++ index.js
@@ -2,8 +2,10 @@ const express = require('express');
 const path = require('path');
 const app = express();
 
+const port = process.env.PORT ?? 4030;
+
 app.use(express.static(path.join(process.cwd(), "public"), {
   extensions: ['html', 'htm']
 }));
 
-app.listen(4030, () => console.log('Frontend server is running'));
+app.listen(port, () => console.log('Frontend server is running'));
