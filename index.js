94b88520608681919e2016fd6c83ca6ca12de19d
vale8293 <34288129+ButterDebugger@users.noreply.github.com>
2024-02-03 20:36:31 -0500

    Added cors

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
 const express = require('express');
 const path = require('path');
+const cors = require('cors');
 const app = express();
 
 const port = process.env.PORT ?? 4030;
+
+app.use(cors());
 
 app.use(express.static(path.join(process.cwd(), "public"), {
   extensions: ['html', 'htm']
