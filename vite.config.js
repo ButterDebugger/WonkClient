bb0f6293d8ec9f8e74b285555ff94aa4425746c7
ButterDebugger <34288129+ButterDebugger@users.noreply.github.com>
2024-06-19 11:21:21 -0400

    Added production server

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,20 @@
-import { resolve } from 'path';
+import { basename, dirname, resolve } from 'node:path';
+import { glob } from 'glob';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
     server: {
         port: 4030
+    },
+    build: {
+        target: "es2022",
+        outDir: resolve(process.cwd(), 'dist'),
+        rollupOptions: {
+            input: (await glob("src/**/index.html")).reduce((put, path) => ({
+                ...put,
+                [basename(dirname(path))]: path
+            }), {}),
+        },
     },
     publicDir: resolve(process.cwd(), 'public'),
     root: resolve(process.cwd(), 'src'),
