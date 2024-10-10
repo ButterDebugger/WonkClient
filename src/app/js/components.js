65945f03e37c5ecfeefbfafcdbce077d02af47d5
ButterDebugger <34288129+ButterDebugger@users.noreply.github.com>
2024-10-10 10:14:58 -0400

    Displayed message attachments

--- src/app/js/components.js
+++ src/app/js/components.js
@@ -1,20 +1,20 @@
 import tippy from "tippy.js";
 import moment from "moment";
 import { dom } from "https://debutter.dev/x/js/dom.js@1.0.0";
+import { client } from "./main";
 
 export function createRoomTab(name) {
 	return dom(`<div class="channel-tab"></div>`)
 		.prop("data-channel-id", name) // NOTE: is name is not always going to be the id
 		.append(
 			`<span class="ic-text-size ic-hashtag"></span>`,
-			dom(`<span name="channel-name"></span>`).text(name)
+			dom(`<span name="channel-name"></span>`).text(name),
 		).element;
 }
 
 export function createUserTag(name, color) {
-	return dom(`<span class="user-tag"></span>`)
-		.text(name)
-		.style("color", color).element;
+	return dom(`<span class="user-tag"></span>`).text(name).style("color", color)
+		.element;
 }
 
 export function createUserChip(name, color, online = true) {
@@ -23,33 +23,45 @@ export function createUserChip(name, color, online = true) {
 	if (!online) tagEle.classList.add("offline");
 
 	return dom(`<div class="user-chip"></div>`).append(
-		`<span class="ic-text-size ic-${
-			online ? "green" : "gray"
-		}-dot"></span>`,
-		tagEle
+		`<span class="ic-text-size ic-${online ? "green" : "gray"}-dot"></span>`,
+		tagEle,
 	);
 }
 
 export function createMessage(message) {
 	// Create timestamp element with tippy
 	let timestampEle = dom(`<span class="message-timestamp"></span>`).text(
-		moment(message.timestamp).format("LT")
+		moment(message.timestamp).format("LT"),
 	).element;
 
 	tippy(timestampEle, {
 		content: moment(message.timestamp).format("LLLL"),
-		theme: "tomato"
+		theme: "tomato",
 	});
+
+	// Create attachment components
+	let attachments = message.attachments.map((attachment) =>
+		createAttachment(attachment),
+	);
 
 	// Return full component
 	return dom(`<div class="message"></div>`).append(
 		dom(`<div class="message-header"></div>`).append(
 			createUserTag(message.author.username, message.author.color),
-			timestampEle
+			timestampEle,
 		),
 		dom(`<div class="message-content"></div>`).append(
 			dom(`<div class="message-body"></div>`).text(message.content),
-			dom(`<div class="message-attachments"></div>`)
-		)
+			dom(`<div class="message-attachments"></div>`).append(...attachments),
+		),
 	).element;
+}
+
+function createAttachment(attachment) {
+	let slashIndex = attachment.lastIndexOf("/");
+	let fileName = attachment.substring(slashIndex + 1);
+
+	return dom(`<a class="file-chip"></a>`)
+		.prop("href", `${client.baseUrl.http}/${attachment}`)
+		.text(fileName);
 }
