8217c396dcd5c35af732c579b0ebef06918f7bd8
ButterDebugger <34288129+ButterDebugger@users.noreply.github.com>
2024-11-07 12:25:27 -0500

    Typed parameters

--- src/lib/cryption.ts
+++ src/lib/cryption.ts
@@ -1,6 +1,6 @@
 import * as openpgp from "openpgp";
 
-export async function decryptMessage(message, privateKey) {
+export async function decryptMessage(message: string, privateKey: string) {
 	const { data: decrypted } = await openpgp.decrypt({
 		message: await openpgp.readMessage({ armoredMessage: message }),
 		decryptionKeys: await openpgp.readKey({ armoredKey: privateKey }),
@@ -9,21 +9,21 @@ export async function decryptMessage(message, privateKey) {
 	return decrypted;
 }
 
-export async function encryptMessage(message, publicKey) {
+export async function encryptMessage(message: string, publicKey: string) {
 	return await openpgp.encrypt({
 		message: await openpgp.createMessage({ text: message }),
 		encryptionKeys: await openpgp.readKey({ armoredKey: publicKey }),
 	});
 }
 
-export async function signMessage(message, privateKey) {
+export async function signMessage(message: string, privateKey: string) {
 	return await openpgp.sign({
 		message: await openpgp.createMessage({ text: message }),
 		signingKeys: await openpgp.readKey({ armoredKey: privateKey }),
 	});
 }
 
-export async function generateKeyPair(name, bits = 2048) {
+export async function generateKeyPair(name: string, bits = 2048) {
 	const { publicKey, privateKey } = await openpgp.generateKey({
 		type: "rsa",
 		rsaBits: bits,
