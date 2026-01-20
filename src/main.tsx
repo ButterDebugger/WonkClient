6971a0fbbc2645b774605a024bc28f8ca9ae885c
ButterDebugger <34288129+ButterDebugger@users.noreply.github.com>
2026-01-20 15:29:07 -0500

    Updated router config

--- src/main.tsx
+++ src/main.tsx
@@ -2,34 +2,23 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import Home from "./pages/Home.tsx";
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router";
 import Privacy from "./pages/Privacy.tsx";
 import Login from "./pages/Login.tsx";
 import App from "./pages/App.tsx";
 import NotFound from "./pages/NotFound.tsx";
 
-const router = createBrowserRouter([
-	{
-		path: "/",
-		element: <Home />,
-		errorElement: <NotFound />,
-	},
-	{
-		path: "/privacy",
-		element: <Privacy />,
-	},
-	{
-		path: "/login",
-		element: <Login />,
-	},
-	{
-		path: "/app",
-		element: <App />,
-	},
-]);
-
 createRoot(document.getElementById("root")!).render(
 	<StrictMode>
-		<RouterProvider router={router} />
+		<BrowserRouter>
+			<Routes>
+				<Route path="*" element={<NotFound />} />
+				{/* Public routes */}
+				<Route path="/" element={<Home />} />
+				<Route path="/privacy" element={<Privacy />} />
+				<Route path="/login" element={<Login />} />
+				<Route path="/app" element={<App />} />
+			</Routes>
+		</BrowserRouter>
 	</StrictMode>,
 );
